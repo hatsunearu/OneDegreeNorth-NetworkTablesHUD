e459aae7b5c4976b38ae33aaed8a288df0553379
hatsunearu <dongimin2@gmail.com>
2014-01-31 13:26:03 +0800

    Create Arduino side sketch

---
+++ HUDArduino/HUDArduino.ino
@@ -0,0 +1,60 @@
+int s;
+
+void setup() {
+	pinMode(6, OUTPUT);
+	pinMode(7, OUTPUT);
+	Serial.begin(9600);
+	digitalWrite(6, LOW);
+	digitalWrite(7, LOW);
+	s = 0;
+}
+
+void loop() {
+	if(Serial.available() > 0) {
+		s = Serial.read();
+	}
+	if(s == 0) {
+		off();
+	}
+	else if (s == 1) {
+		left();
+	}
+	else if (s == 2) {
+		right();
+	}
+	else if (s == 3) {
+		both();
+	}
+	
+}
+
+void left() {
+	digitalWrite(6, HIGH);
+	digitalWrite(7, LOW);
+	delay(50);
+	digitalWrite(6, LOW);
+	delay(50);
+}
+
+void right() {
+	digitalWrite(6, LOW);
+	digitalWrite(7, HIGH);
+	delay(50);
+	digitalWrite(7, LOW);
+	delay(50);
+}
+
+void off() {
+	digitalWrite(6, LOW);
+	digitalWrite(7, LOW);
+	delay(100);
+}
+
+void both() {
+	digitalWrite(6, LOW);
+	digitalWrite(7, HIGH);
+	delay(50);
+	digitalWrite(6, HIGH);
+	digitalWrite(7, LOW);
+	delay(50);
+}
